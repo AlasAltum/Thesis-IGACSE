9b8c6ca4904974efde10264f056122f33ebeff65
AlasAltum <alonso.utreras@ug.uchile.cl>
2022-10-19 22:41:27 -0300

    Moved some files, added more explanations to lines in kruskal and DFS and added a background texture

--- Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/adt_is_not_empty_popup.gd
+++ Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/adt_is_not_empty_popup.gd
@@ -1,12 +1,22 @@
 class_name ADTIsEmptyPopup
 extends WindowDialog
 
+export (String) var ADT_type_and_var_name = "Queue q"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	NotificationManager.adt_is_empty_popup = self
 	var close_button : TextureButton = get_close_button()
 	close_button.visible = false
+	set_text(self.ADT_type_and_var_name)
+
+func set_text(ADT_type_name: String) -> void:
+	var explanation_label: Label = $Explanation
+	var explanation = "Is the {ADT_type_name} empty?"
+	explanation = explanation.format({"ADT_type_name": ADT_type_name})
+	explanation_label.text = explanation
+
 
 func _on_adt_is_empty_YesButton_pressed() -> void:
 	NotificationManager._on_adt_is_empty_YesButton_pressed()
