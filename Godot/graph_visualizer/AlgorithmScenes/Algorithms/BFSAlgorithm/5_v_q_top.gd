3129963672e38f6ec28e91c63c1f4361abfc7572
AlasAltum <alonso.utreras@ug.uchile.cl>
2022-11-20 00:00:45 -0300

    Added sound for v.pop

--- Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/5_v_q_top.gd
+++ Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/5_v_q_top.gd
@@ -1,6 +1,10 @@
 extends EffectCheck
 # v = q.top()
 
+var audio: AudioStreamPlayer = null
+var pop_stream: AudioStreamOGGVorbis = preload("res://AlgorithmScenes/Audio/pop.ogg")
+
+
 func check_actions_correct() -> bool:
 	return true  # This is not required
 
@@ -10,6 +14,20 @@ func execute_side_effect() -> void:
 		var top_node_in_queue : AGraphNode = queue.top()
 		if top_node_in_queue:
 			StoredData.add_variable("v", top_node_in_queue.get_adt())
+			_play_pop_sound()
+
+func _play_pop_sound():
+	audio = AudioStreamPlayer.new()
+	StoredData.add_child(audio)
+	pop_stream.set_loop(false)
+	audio.stream = pop_stream
+	audio.play()
+
+func _stop_pop_sound():
+	audio.stop()
+	StoredData.remove_child(audio)
+
+
 
 func get_next_line() -> int:
 	self.execute_side_effect()
--- Godot/graph_visualizer/README.md
+++ Godot/graph_visualizer/README.md
@@ -1,6 +1,6 @@
 # Graph Visualizer
 
-An educative interactive application to learn graph-related algorithms such as BFS, DFS, Kruskal and Prim.
+An educative application to learn graph-related algorithms such as BFS, DFS, Kruskal and Prim.
 This application was made in Godot using mainly GDScript. C# was used in the beggining, but it does not allow to export games to HTML5, so it is not being used anymore.
 The purpose of this job is to analyze if this interactive application help new CS students to learn new algorithms and data structures.
 
