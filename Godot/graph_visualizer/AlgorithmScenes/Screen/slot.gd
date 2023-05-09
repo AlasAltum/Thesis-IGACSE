4d04e1974bb2a15084751f37ca2cf5eb00aa90a6
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-05-09 01:06:38 -0400

    Fixed focus error with slots and adts

--- Godot/graph_visualizer/AlgorithmScenes/Screen/slot.gd
+++ Godot/graph_visualizer/AlgorithmScenes/Screen/slot.gd
@@ -1,5 +1,5 @@
 class_name Slot
-extends Area2D
+extends Container
 
 # A slot contains a Data Structure.
 # If the user presses a slot with a data structure, a popup
@@ -10,7 +10,7 @@ export (Texture) var slot_texture
 export (Resource) var generated_adt  # The ADT for the corresponding slot
 export (PackedScene) var on_hover_animation
 
-onready var texture : TextureRect = $ADTTexture
+onready var texture : TextureRect = $SlotTexture/ADTTexture
 onready var hover_panel : Node2D = $HoverPanel
 onready var hover_label : Label = $HoverPanel/ColorRect/HoverText
 onready var ADT_name : Label = $SlotTexture/BelowADTName
@@ -30,21 +30,12 @@ func _ready():
 		$HoverPanel/ColorRect/AnimationPlaceholder.queue_free()
 		ADT_name.text = hover_text
 
-	if slot_texture:
+	if slot_texture and texture:
 		texture.texture = slot_texture
 
-#func _mouse_is_inside_rect() -> bool:
-#	return get_rect().has_point(get_global_mouse_position())
-#
-#func _process(delta):
-#	if _mouse_is_inside_rect():
-#		_on_Area2D_mouse_entered()
-##		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-##			_create_adt()
-#	else:
-#		_on_Area2D_mouse_exited()
-#
-#
+func _mouse_is_inside_rect() -> bool:
+	return $SlotTexture.get_rect().has_point(get_global_mouse_position())
+
 func _on_Area2D_mouse_entered():
 	hover_panel.visible = true
 	if hover_animation_instance:
@@ -57,22 +48,13 @@ func _on_Area2D_mouse_exited():
 
 # When an ADT is left clicked on the menu, create an internal resource
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
-	if (event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed):
+	if _event_is_click(event):
 		_create_adt()
 
 func _create_adt():
+	if generated_adt:
 		StoredData.adt_to_be_created = generated_adt.new()
 		NotificationManager._on_variable_creation_popup(ADT_name.text)
 
-
-func _on_Slot_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		_create_adt()
-
-
-func _on_Slot_mouse_entered():
-	_on_Area2D_mouse_exited()
-
-
-func _on_Slot_mouse_exited():
-	_on_Area2D_mouse_exited()
+func _event_is_click(event) -> bool:
+	return event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed
