1a4c606180b286dfd8b5baafa8d252f2a285f1a8
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-05-17 00:18:58 -0400

    Transition from introductory dialogue to first level is ready

--- Godot/graph_visualizer/GameFlow/story_mode_scene.gd
+++ Godot/graph_visualizer/GameFlow/story_mode_scene.gd
@@ -8,11 +8,9 @@ var animation_node : AnimationPlayer
 
 
 func _ready():
-	var animation_node = null
-	if animation_node_path:
-		animation_node = get_node(animation_node_path)
-		animation_node.play("FadeIn")
-		StoredData.world_node = self
+	animation_node = $FadeInOut
+	animation_node.play("FadeIn")
+	StoredData.world_node = self
 
 func _on_FadeInOut_animation_finished(anim_name):
 	if anim_name == "FadeIn":
@@ -21,10 +19,7 @@ func _on_FadeInOut_animation_finished(anim_name):
 			dialogue_shower.show_first_dialogue()
 
 	if anim_name == "FadeOut":
-		var dialogue_shower: DialogueDisplayer = $CanvasLayer/DialogueShower
-		if dialogue_shower:
-			dialogue_shower.visible = false
-			go_to_scene("res://AlgorithmScenes/Algorithms/BFSAlgorithmTutorial/BFS_tutorial.tscn")
+		go_to_scene("res://AlgorithmScenes/Algorithms/BFSAlgorithmTutorial/BFS_tutorial.tscn")
 
 func go_to_scene(scene_path: String):
 	call_deferred("deferred_goto_scene", scene_path)
@@ -32,15 +27,13 @@ func go_to_scene(scene_path: String):
 func deferred_goto_scene(scene_path: String):
 	self.queue_free()
 	var next_scene = load(scene_path)
-	get_tree().get_root().add_child(next_scene) 
+	var next_scene_instance = next_scene.instance()
+	get_tree().root.add_child(next_scene_instance) 
 
 func _on_DialogueShower_dialogue_finished():
-	$LaunchingSound.play()
 	$Background/Part1Background.ship_movement_speed += 0.2
 	$Background/Part1Background.dialogue_finished = true
 	if animation_node:
 		animation_node.play("FadeOut")
+		$LaunchingSound.play()
 
-
-
-	
