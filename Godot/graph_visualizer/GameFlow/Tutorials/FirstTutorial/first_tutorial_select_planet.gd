6a0800f62034d2594dc3275eb9008b0bdab89ad0
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-07-13 23:50:39 -0400

    Add sun animation

--- Godot/graph_visualizer/GameFlow/Tutorials/FirstTutorial/first_tutorial_select_planet.gd
+++ Godot/graph_visualizer/GameFlow/Tutorials/FirstTutorial/first_tutorial_select_planet.gd
@@ -21,7 +21,9 @@ func _ready():
 	planet1.animation_player.connect("animation_finished", self, "on_ship_arrived_to_planet")
 	planet2.animation_player.connect("animation_finished", self, "on_ship_arrived_to_planet")
 	dialogue_displayer.connect("dialogue_finished", self, "_on_DialogueShower_dialogue_finished")
-	
+	$SunMovement.play("PlanetMovement")
+
+
 func send_ship_to_node(end_planet: AGraphNode):
 	# Since there is always only one edge, this should work fine
 	var edge #  = $ColorRect/Edge1S:
