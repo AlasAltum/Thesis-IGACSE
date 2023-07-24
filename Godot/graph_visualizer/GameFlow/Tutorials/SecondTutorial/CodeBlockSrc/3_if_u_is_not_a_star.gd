339bfb06aa626483f53ebe36a51a6c36d159ae47
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-07-23 23:57:11 -0400

    Second tutorial: Change name of the variable from node to u. Add script for if

---
+++ Godot/graph_visualizer/GameFlow/Tutorials/SecondTutorial/CodeBlockSrc/3_if_u_is_not_a_star.gd
@@ -0,0 +1,8 @@
+extends EffectCheck
+# if u is not a star
+
+func effect_check_on_focused():
+	# Check if the curent node to select is a star or not
+	# Show a popup asking if the node u is a star.
+	# If the user answers wrongly, the ship will fly to the sun.
+	return
