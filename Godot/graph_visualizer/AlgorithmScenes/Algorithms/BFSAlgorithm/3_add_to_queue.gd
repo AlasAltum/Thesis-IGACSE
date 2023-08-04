44486b91846430c287cdf6c6a89e854bb8f4af89
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-08-04 02:05:13 -0400

    Fix third line of new BFS: Add to queue

--- Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/3_add_to_queue.gd
+++ Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/3_add_to_queue.gd
@@ -1,5 +1,9 @@
 extends EffectCheck
 
+func effect_check_on_focused() -> void:
+	var t : AGraphNode = StoredData.get_variable("t").get_node()
+	StoredData.add_node_to_nodes_that_should_be_added_to_adt(t)
+
 # Make sure that user has created a queue with name q and it has the node(0). 
 func check_actions_correct() -> bool:
 	if StoredData.has_variable("q") and StoredData.has_variable("t"):
