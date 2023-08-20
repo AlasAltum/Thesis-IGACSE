7af4c0a9752d8a2ce6491205e84c46f572d55e7d
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-08-20 17:53:08 -0400

    DFS: The player must press node 0 to pass first line t = starting_node

--- Godot/graph_visualizer/AlgorithmScenes/Algorithms/DFSAlgorithm/1_take_node_0.gd
+++ Godot/graph_visualizer/AlgorithmScenes/Algorithms/DFSAlgorithm/1_take_node_0.gd
@@ -8,26 +8,28 @@ func effect_check_on_focused() -> void:
 	t.highlight_node()
 
 	if StoredData.world_node: # : SecondTutorial
-		for _node in StoredData.nodes:
-			if is_instance_valid(_node) and _node.index == 0:
-				node_0 = _node
-				StoredData.selectable_nodes_indexes.append(0)
-				break
+		node_0 = StoredData.get_node_by_index(0)
+		StoredData.selectable_nodes_indexes.append(0)
 
 	helping_timer = Timer.new()
 	StoredData.world_node.add_child(helping_timer)
 	helping_timer.start(5)
 	helping_timer.connect("timeout", self, "on_timeout_help_user")
 
+func check_actions_correct():
+	if is_instance_valid(node_0):
+		if node_0.selected:
+			return true
+	return false
 
 func _trigger_on_correct_once() -> void:
 	if helping_timer and is_instance_valid(helping_timer):
 		helping_timer.queue_free()
-	
+
 func on_timeout_help_user():
 	if node_0 and is_instance_valid(helping_timer):
 		node_0.show_animation_of_clicking_mouse()
-	
+
 func _trigger_on_next_line_side_effect() -> void:
 	var t = StoredData.nodes[0]
 	StoredData.add_variable("t", t.get_adt())
