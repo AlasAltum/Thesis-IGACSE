3c5f11c301cd4a8896d1ea851019b219fc088f86
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-09-08 15:35:45 -0300

    Third tutorial shows hint to add nodes to ADT

--- Godot/graph_visualizer/GameFlow/Tutorials/ThirdTutorial/CodeBlockSrc/5_add_to_stack_third_tutorial.gd
+++ Godot/graph_visualizer/GameFlow/Tutorials/ThirdTutorial/CodeBlockSrc/5_add_to_stack_third_tutorial.gd
@@ -23,3 +23,18 @@ func check_actions_correct() -> bool:
 func _trigger_on_next_line_side_effect():
 	var dialogue_displayer = StoredData.world_node.dialogue_displayer
 	dialogue_displayer.close_single_text()
+
+func _show_hint_to_user():
+	var t : AGraphNode = StoredData.get_variable("t").get_node()
+	t.show_animation_of_R()
+	# Get s label and highlight it with a looping animation
+	# deactivate the animation once the user navigates to the next line
+	StoredData.highlight_variable("s", true)
+
+func _trigger_on_correct_once():
+	._trigger_on_correct_once()
+	StoredData.stop_highlight_variable("s")
+	var t : AGraphNode = StoredData.get_variable("t").get_node()
+	t.stop_animation_of_R()
+
+
--- Godot/graph_visualizer/GameFlow/Tutorials/ThirdTutorial/CodeBlockSrc/6_add_to_stack_third_tutorial.gd
+++ Godot/graph_visualizer/GameFlow/Tutorials/ThirdTutorial/CodeBlockSrc/6_add_to_stack_third_tutorial.gd
@@ -12,3 +12,16 @@ func check_actions_correct() -> bool:
 		if StoredData.get_variable("s").as_string() == "Stack((0), (2))":
 			return true
 	return false
+
+func _show_hint_to_user():
+	var node_2 : AGraphNode = StoredData.get_node_by_index(2)
+	node_2.show_animation_of_R()
+	# Get s label and highlight it with a looping animation
+	# deactivate the animation once the user navigates to the next line
+	StoredData.highlight_variable("s", true)
+
+func _trigger_on_correct_once():
+	._trigger_on_correct_once()
+	StoredData.stop_highlight_variable("s")
+	var node_2 : AGraphNode = StoredData.get_node_by_index(2)
+	node_2.stop_animation_of_R()
