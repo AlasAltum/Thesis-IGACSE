3f91b6b3a36519a988c0d9baa9d897634b45cbc1
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-11-10 20:20:14 -0300

    Fixed questions according to manual and not paper

--- Tesis/CodeToInspectResults/graph_results.py
+++ Tesis/CodeToInspectResults/graph_results.py
@@ -3,15 +3,69 @@ Script intended to create graph results analyzing the answers of the users.
 Form URL: https://forms.gle/6JXo8FmPmmarP2AA6
 """
 
+import os
 
-try:
-    import pandas as pd
-    OUTPUT_FILENAME = "ProcesamientoArchivos.csv"
-    output_df = pd.read_csv(OUTPUT_FILENAME)
-except: 
-    from preprocess_results import output_df
-
-# output_df has everything we need to graph
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
- 
+
+INPUT_CSV_FILE = "SumOfResultsIGASCE.csv"
+OUTPUT_GRAPH_NAME = "answers.png"
+
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+csv_full_file_path = os.path.join(current_directory, INPUT_CSV_FILE)
+"""
+Read the data and transform it into a dataframe. We use the following format:
+
+Questions: Q1; Q2; Q3, .... Q31
+Strongly disagree: 0, 2, 5, ...
+Disagree: 2, 1, 3, ...
+Not disagree nor agree: 5, 1, 2, ...
+Agree: 3, 2, 0, ...
+Strongly agree: 5, 2, 4, ...
+"""
+
+df = pd.read_csv(csv_full_file_path, delimiter=';')
+
+# We want to plot: for each question, the number of times each category was marked.
+categories = [
+    'Muy en desacuerdo',
+    'En desacuerdo',
+    'Ni en desacuerdo ni de acuerdo',
+    'De acuerdo',
+    'Muy de acuerdo',
+]
+questions_indexes = [f'Q{i+1}' for i in range(31)]
+values = df.loc[:, 'Q1':'Q31'].values  # Numpy array that can be plotted with bars, it has all the values
+
+plt.figure(figsize=(15, 6))
+bar_width = 0.15
+q_indexes = np.arange(len(questions_indexes))
+
+# Show the 31 bars for each category
+for i, category in enumerate(categories):
+    bars = plt.bar(
+        x=q_indexes + i * bar_width,
+        height=values[i, :],
+        width=bar_width,
+        label=category
+    )
+
+
+# Customize the plot
+plt.xlabel('Pregunta y Categoría')
+plt.ylabel('Número de respuestas')
+plt.title('Número de respuestas por categoría para cada pregunta')
+plt.xticks(q_indexes + bar_width * 2, questions_indexes)  # Items: Show Q1, Q2, Q3... Q31
+plt.legend(title='Categorías')
+
+# Show the plot
+plt.tight_layout()
+plt.show()
+
+plt.savefig(OUTPUT_GRAPH_NAME, bbox_inches='tight')
+
+# Print the path where the plot is saved
+print(f'Plot saved as: {OUTPUT_GRAPH_NAME}')
