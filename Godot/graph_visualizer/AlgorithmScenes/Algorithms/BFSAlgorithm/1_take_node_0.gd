aa169cc9ec6c7b41c12763aa5ac2156cbeee3e86
AlasAltum <alonso.utreras@ug.uchile.cl>
2025-04-11 23:30:42 -0400

    Now node_t doesn't dissapear after first line in BFS has been executed

--- Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/1_take_node_0.gd
+++ Godot/graph_visualizer/AlgorithmScenes/Algorithms/BFSAlgorithm/1_take_node_0.gd
@@ -6,6 +6,8 @@ func check_actions_correct() -> bool:
 	var selected_nodes = StoredData.get_selected_nodes()
 	if selected_nodes.size() == 1 && selected_nodes[0].index == 0:
 		StoredData.add_variable("t", selected_nodes[0].get_adt())
+		var node_t = StoredData.get_node_by_index(0) # AGraphNode
+		node_t.highlight_variable("t")
 		return true
 	return false
 
@@ -20,10 +22,12 @@ func effect_check_on_focused():
 		node_t.highlight_node()
 		node_t.show_ship_flying_around()
 
+
 ## In tutorial version, play animations that display elements one by one
 func _trigger_on_next_line_side_effect() -> void:
 	if NotificationManager.animation_player:
 		NotificationManager.animation_player.show_variables_panel()
 	var node_t = StoredData.get_node_by_index(0) # AGraphNode
 	if node_t:
-		node_t.unhighlight_node()
+		node_t.highlight_variable("t")
+#		node_t.unhighlight_node()
